cd2dbec82b226857d16e84367c588719c1a3965b
Luciana-CN <luciana-cani@live.com.ar>
2021-04-25 08:40:26 -0300

    Subo la clase Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,37 @@
+package ar.edu;
+
+import java.util.Scanner;
+
+public class Calculadora {
+	
+	public static void main(String[] args) {
+        Integer a;
+        Integer b;
+        Integer opcion;
+        Integer resultado;
+        Scanner teclado = new Scanner(System.in);
+
+
+        do {
+            System.out.println("Ingrese su operación / 1 para sumar / 2 para restar / 3 para multiplicar / 4 para dividir");
+            opcion = teclado.nextInt();
+        } while (opcion < 1 && opcion > 4);
+
+        System.out.println("Ingrese el primer numero");
+        a = teclado.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        b = teclado.nextInt();
+
+        if (opcion.equals(1)) {
+            resultado = a + b;
+        } else if (opcion.equals(2)) {
+            resultado = a - b;
+        } else if (opcion.equals(3)) {
+            resultado = a * b;
+        } else {
+            resultado = a / b;
+        }
+        System.out.println("El resultado es " + resultado.toString());
+    }
+
+}
